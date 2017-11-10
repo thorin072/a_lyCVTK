3e50973252966aa09de48ec9c71838c44ae17710
Daniel Varfolomeev <dvarfolomeev04@gmail.com>
2017-11-10 21:51:41 +0300

    Initial commit.

---
+++ CVTK/CVTK/canny.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CVTK
+{
+    partial class canny
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericth2 = new System.Windows.Forms.NumericUpDown();
+            this.numericth1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericth2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericth1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericth2);
+            this.groupBox1.Controls.Add(this.numericth1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 193);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройка фильтра";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 20);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "null";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Кол-во контуров:";
+            // 
+            // numericth2
+            // 
+            this.numericth2.Location = new System.Drawing.Point(80, 47);
+            this.numericth2.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericth2.Name = "numericth2";
+            this.numericth2.Size = new System.Drawing.Size(104, 20);
+            this.numericth2.TabIndex = 6;
+            // 
+            // numericth1
+            // 
+            this.numericth1.Location = new System.Drawing.Point(80, 21);
+            this.numericth1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericth1.Name = "numericth1";
+            this.numericth1.Size = new System.Drawing.Size(104, 20);
+            this.numericth1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Граница 1: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Граница 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Аппрокс:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 20);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "0,0009";
+            // 
+            // canny
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(220, 214);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "canny";
+            this.Text = "Настройки контуров";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericth2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericth1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericth2;
+        private System.Windows.Forms.NumericUpDown numericth1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ CVTK/CVTK/canny.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.Util;
+using Emgu.CV.Structure;
+namespace CVTK
+{
+    public partial class canny : Form
+    {
+    
+        MainCV _main;
+        
+        public canny()
+        {
+            InitializeComponent();
+        }
+        public canny(MainCV hm)
+        {
+            InitializeComponent();
+            _main = hm;
+
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(_main != null)
+            {
+                double a;
+                double.TryParse(textBox1.Text,out a);
+                _main.ApplyCanny((double)numericth1.Value, (double)numericth2.Value, a);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
