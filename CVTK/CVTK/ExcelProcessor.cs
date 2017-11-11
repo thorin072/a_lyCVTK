ff625c90d7d08ff33f63efd49efc5c25e0ce3a9e
thorin072 <33495916+thorin072@users.noreply.github.com>
2017-11-11 11:43:39 +0300

    логика для экселя

---
+++ CVTK/CVTK/ExcelProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CVTK
+{
+    public static class ExcelProcessor
+    {
+
+        public static void Pointtofile(int[] x, int[] y)
+        {
+            // Создаём экземпляр приложения
+            Excel.Application excelApp = new Excel.Application();
+            // Создаём экземпляр рабочий книги Excel
+            Excel.Workbook workBook;
+            // Создаём экземпляр листа Excel
+            Excel.Worksheet workSheet;
+            workBook = excelApp.Workbooks.Add();
+            workSheet = (Excel.Worksheet)workBook.Worksheets.get_Item(1);
+
+            for (int i = 0; i <= x.Length - 1; i++)
+            {
+                workSheet.Cells[i + 1, 1] = x[i];
+            }
+            for (int i = 0; i <= y.Length - 1; i++)
+            {
+                workSheet.Cells[i + 1, 2] = y[i];
+            }
+            string outpath = Environment.CurrentDirectory+ "/";
+            workBook.SaveAs(@outpath + "end_points.xlsx");
+            workBook.Close();
+            excelApp.Quit();
+        }
+    }
+}
