067f59198db08ac363e8c2f2ba3c5a7ab7ed9500
thorin072 <33495916+thorin072@users.noreply.github.com>
2017-12-02 15:01:19 +0300

    update

--- CVTK/CVTK/ExcelProcessor.cs
+++ CVTK/CVTK/ExcelProcessor.cs
@@ -30,25 +30,31 @@ namespace CVTK
             Excel.Worksheet workSheet;
             workBook = excelApp.Workbooks.Add();
             workSheet = (Excel.Worksheet)workBook.Worksheets.get_Item(1);
+            workSheet.Range["A2:A10000"].NumberFormat = "0.E+00";
             workSheet.Cells[1, 1] = "t";
             workSheet.Cells[1, 2] = "x";
             workSheet.Cells[1, 3] = "y";//Z
             workSheet.Cells[1, 4] = "z";
             workSheet.Cells[2, 1] = 0*1e-3;
+            workSheet.Cells[2, 4] = 0 * 1e-3;
+            workSheet.Cells[2, 2] =points[0].Contr[0].Y;
+            workSheet.Cells[2, 3] = 686.6;
             //****Файл инициализирован****
 
             //****Разбиение на компоненты
 
             //---time-— 
             double time = 1 * 1e-3;
-            int count = 2;
+            int count = 3;
+            double u = 686.6;
             for (int i = 0; i < points.Count; i++) // Коллекция I - ых контуров ( количество найденых 0..n)
             {
                 for (int m = 0; m < points[i].Contr.Count; m++) // Обращение к элементам коллекции points[i].Contr 
                 {
                     workSheet.Cells[count, 1] = time;
                     workSheet.Cells[count, 4] = points[i].Contr[m].X; //------для X = Z —--— 
-                    workSheet.Cells[count, 2] = points[i].Contr[m].Y; //------для Y = X----— 
+                    workSheet.Cells[count, 2] = points[i].Contr[m].Y; //------для Y = X----—  =C$2-(0,00120148148148148*(1000*A3)^2)/2
+                    workSheet.Cells[count, 3] = u - (0.00120148148148148 * Math.Pow((1000 * time), 2))/ 2;
                     if ((points[i].Contr[m].X == points[i].Mass.X) && (points[i].Contr[m].Y == points[i].Mass.Y))
                     {
                         workSheet.Cells[count, 3] = 0; // проверка
