857e68ba694be2549656cc56a1249aa70e5a0e8d
thorin072 <33495916+thorin072@users.noreply.github.com>
2017-12-02 16:17:12 +0300

    update

--- CVTK/CVTK/maintk.Designer.cs
+++ CVTK/CVTK/maintk.Designer.cs
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainCV));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label9 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -63,10 +62,11 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.MidnightBlue;
             this.label9.Location = new System.Drawing.Point(329, 461);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(181, 17);
+            this.label9.Size = new System.Drawing.Size(201, 16);
             this.label9.TabIndex = 15;
             this.label9.Text = "Визуализация найденных контуров";
             // 
@@ -108,23 +108,18 @@
             // 
             // visualgraph
             // 
-            chartArea2.Name = "ChartArea1";
-            this.visualgraph.ChartAreas.Add(chartArea2);
+            chartArea1.Name = "ChartArea1";
+            this.visualgraph.ChartAreas.Add(chartArea1);
             this.visualgraph.Location = new System.Drawing.Point(149, 24);
             this.visualgraph.Name = "visualgraph";
             this.visualgraph.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
             this.visualgraph.PaletteCustomColors = new System.Drawing.Color[] {
         System.Drawing.Color.Blue};
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series3.Color = System.Drawing.Color.RoyalBlue;
-            series3.Name = "Series1";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series4.Color = System.Drawing.Color.Red;
-            series4.Name = "Series2";
-            this.visualgraph.Series.Add(series3);
-            this.visualgraph.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Color = System.Drawing.Color.RoyalBlue;
+            series1.Name = "Series1";
+            this.visualgraph.Series.Add(series1);
             this.visualgraph.Size = new System.Drawing.Size(538, 454);
             this.visualgraph.TabIndex = 23;
             this.visualgraph.Text = "visualgraph";
@@ -132,7 +127,7 @@
             // ysm
             // 
             this.ysm.AutoSize = true;
-            this.ysm.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ysm.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ysm.Location = new System.Drawing.Point(21, 98);
             this.ysm.Name = "ysm";
             this.ysm.Size = new System.Drawing.Size(32, 16);
@@ -142,7 +137,7 @@
             // xsm
             // 
             this.xsm.AutoSize = true;
-            this.xsm.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xsm.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.xsm.Location = new System.Drawing.Point(21, 59);
             this.xsm.Name = "xsm";
             this.xsm.Size = new System.Drawing.Size(32, 16);
@@ -163,7 +158,7 @@
             0,
             0});
             this.valueY.Name = "valueY";
-            this.valueY.Size = new System.Drawing.Size(69, 21);
+            this.valueY.Size = new System.Drawing.Size(69, 22);
             this.valueY.TabIndex = 1;
             this.valueY.Value = new decimal(new int[] {
             100,
@@ -186,7 +181,7 @@
             0,
             0});
             this.valueX.Name = "valueX";
-            this.valueX.Size = new System.Drawing.Size(69, 21);
+            this.valueX.Size = new System.Drawing.Size(69, 22);
             this.valueX.TabIndex = 0;
             this.valueX.Value = new decimal(new int[] {
             100,
@@ -198,20 +193,20 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label6.Location = new System.Drawing.Point(9, 38);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(104, 16);
+            this.label6.Size = new System.Drawing.Size(105, 16);
             this.label6.TabIndex = 3;
             this.label6.Text = "X                          px";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label7.Location = new System.Drawing.Point(11, 77);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 16);
+            this.label7.Size = new System.Drawing.Size(102, 16);
             this.label7.TabIndex = 4;
             this.label7.Text = "Y                         px";
             // 
@@ -227,60 +222,63 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(6, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.Size = new System.Drawing.Size(148, 16);
             this.label1.TabIndex = 28;
             this.label1.Text = "Размеры рабочей области: ";
             // 
             // infoex
             // 
             this.infoex.AutoSize = true;
-            this.infoex.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoex.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoex.ForeColor = System.Drawing.Color.DarkBlue;
             this.infoex.Location = new System.Drawing.Point(32, 191);
             this.infoex.Name = "infoex";
-            this.infoex.Size = new System.Drawing.Size(60, 16);
+            this.infoex.Size = new System.Drawing.Size(62, 16);
             this.infoex.TabIndex = 6;
             this.infoex.Text = "Не создан";
             // 
             // infopoint
             // 
             this.infopoint.AutoSize = true;
-            this.infopoint.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infopoint.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infopoint.ForeColor = System.Drawing.Color.Brown;
             this.infopoint.Location = new System.Drawing.Point(11, 168);
             this.infopoint.Name = "infopoint";
-            this.infopoint.Size = new System.Drawing.Size(11, 16);
+            this.infopoint.Size = new System.Drawing.Size(12, 16);
             this.infopoint.TabIndex = 5;
             this.infopoint.Text = "-";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label8.Location = new System.Drawing.Point(11, 155);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(94, 16);
+            this.label8.Size = new System.Drawing.Size(95, 16);
             this.label8.TabIndex = 4;
             this.label8.Text = "Кол-во контуров:";
             // 
             // infosize
             // 
             this.infosize.AutoSize = true;
-            this.infosize.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infosize.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infosize.ForeColor = System.Drawing.Color.Brown;
             this.infosize.Location = new System.Drawing.Point(11, 135);
             this.infosize.Name = "infosize";
-            this.infosize.Size = new System.Drawing.Size(11, 16);
+            this.infosize.Size = new System.Drawing.Size(12, 16);
             this.infosize.TabIndex = 2;
             this.infosize.Text = "-";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(11, 122);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 16);
+            this.label2.Size = new System.Drawing.Size(117, 16);
             this.label2.TabIndex = 0;
             this.label2.Text = "Размер изображения:";
             // 
@@ -299,7 +297,7 @@
             this.groupBox1.Controls.Add(this.valueY);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox1.Location = new System.Drawing.Point(12, 36);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(161, 227);
