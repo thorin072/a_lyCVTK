52f0e58edc5d31fec6a74e818b0d201b29ecdeed
thorin072 <33495916+thorin072@users.noreply.github.com>
2018-02-04 11:58:16 +0300

    +++ для откуда точка

--- CVTK/CVTK/maintk.Designer.cs
+++ CVTK/CVTK/maintk.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainCV));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label9 = new System.Windows.Forms.Label();
             this.menu = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -87,6 +87,9 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.listView1 = new System.Windows.Forms.ListView();
             this.splitter1 = new System.Windows.Forms.Splitter();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.menu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.visualgraph)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.valueY)).BeginInit();
@@ -126,7 +129,7 @@
             this.menu.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.menu.Location = new System.Drawing.Point(0, 0);
             this.menu.Name = "menu";
-            this.menu.Size = new System.Drawing.Size(680, 24);
+            this.menu.Size = new System.Drawing.Size(667, 24);
             this.menu.TabIndex = 19;
             this.menu.Text = "menuStrip1";
             // 
@@ -177,27 +180,27 @@
             // 
             this.visualgraph.BackColor = System.Drawing.Color.Transparent;
             this.visualgraph.BorderlineColor = System.Drawing.SystemColors.ButtonFace;
-            chartArea2.Name = "ChartArea1";
-            this.visualgraph.ChartAreas.Add(chartArea2);
+            chartArea7.Name = "ChartArea1";
+            this.visualgraph.ChartAreas.Add(chartArea7);
             this.visualgraph.Location = new System.Drawing.Point(226, 35);
             this.visualgraph.Name = "visualgraph";
             this.visualgraph.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
             this.visualgraph.PaletteCustomColors = new System.Drawing.Color[] {
         System.Drawing.Color.Blue};
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series3.Color = System.Drawing.Color.DarkViolet;
-            series3.MarkerSize = 8;
-            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
-            series3.Name = "Series1";
-            series3.YValuesPerPoint = 2;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series4.Color = System.Drawing.Color.MidnightBlue;
-            series4.MarkerSize = 3;
-            series4.Name = "Series2";
-            this.visualgraph.Series.Add(series3);
-            this.visualgraph.Series.Add(series4);
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series13.Color = System.Drawing.Color.DarkViolet;
+            series13.MarkerSize = 8;
+            series13.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series13.Name = "Series1";
+            series13.YValuesPerPoint = 2;
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series14.Color = System.Drawing.Color.MidnightBlue;
+            series14.MarkerSize = 3;
+            series14.Name = "Series2";
+            this.visualgraph.Series.Add(series13);
+            this.visualgraph.Series.Add(series14);
             this.visualgraph.Size = new System.Drawing.Size(441, 379);
             this.visualgraph.TabIndex = 23;
             this.visualgraph.Text = "visualgraph";
@@ -408,7 +411,7 @@
             this.infoexcel});
             this.statusStrip1.Location = new System.Drawing.Point(0, 637);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(680, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(667, 22);
             this.statusStrip1.TabIndex = 29;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -747,9 +750,9 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(251, 400);
+            this.listView1.Location = new System.Drawing.Point(252, 409);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(416, 174);
+            this.listView1.Size = new System.Drawing.Size(399, 165);
             this.listView1.SmallImageList = this.imageList1;
             this.listView1.TabIndex = 48;
             this.listView1.UseCompatibleStateImageBehavior = false;
@@ -763,12 +766,42 @@
             this.splitter1.TabIndex = 49;
             this.splitter1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 580);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 50;
+            this.button3.Text = "Удалить из списка";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(252, 580);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 23);
+            this.button4.TabIndex = 51;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(521, 580);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 23);
+            this.button5.TabIndex = 52;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // MainCV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(680, 659);
+            this.ClientSize = new System.Drawing.Size(667, 659);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.panel1);
@@ -867,6 +900,9 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- CVTK/CVTK/maintk.cs
+++ CVTK/CVTK/maintk.cs
@@ -7,7 +7,6 @@ using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
-using System.Drawing;
 using System.Collections.Generic;
 using Accord.Imaging;
 using Accord.Imaging.Filters;
@@ -21,11 +20,20 @@ namespace CVTK
         Image<Gray, byte> img2;
         IEnumerable<RobotCommand.RobotPosition> ExcelArr;
 
+        public class ContourFromTree
+        {
+            /// <summary>
+            /// Лист точек контура
+            /// </summary>
+            public List<Point> Contr;
+            public string namecontour;
+        }
         public class InfoDelete
         {
             public string parent;
             public int counts;
         }
+
 
         public MainCV()
         {
@@ -41,33 +49,25 @@ namespace CVTK
         private void FindContours(Image<Gray, byte> bin)
         {
             var points = CentroMass.DeterminationOfCentromass(bin, ChainApproxMethod.ChainApproxNone);
-            // var key = KeyPoint.SortTopPoint(points);
 
 
-            //вывод графа все точек и всех контуров 
-            TreePick(points);
-            List<CentroMass.ContourWithMass> SortedList = points.OrderBy(j => j.Mass.X).ToList(); // сортировка по центру масс (х)
+            TreePick(points); //вывод графа все точек и всех контуров 
+
+            var ImgCorner = img2.Bitmap;
+            CornerFound(ImgCorner, 0.04, 20000, 1.4);
+
+            //List<CentroMass.ContourWithMass> SortedList = points.OrderBy(j => j.Mass.X).ToList(); // сортировка по центру масс (х)
             infopcontr.Text = points.Count.ToString();
 
-            //var visualXY = new List<Point>();
-            //////Заполнение массивов для графика визуализации
-            //for (int i = 0; i < SortedList.Count; i++)
-            //{
-            //    visualXY.AddRange(SortedList[i].Contr.ToArray());
-            //}
-            //var x = visualXY.Select(_ => _.X).ToArray();
-            //infopoint.Text = x.Length.ToString();
-            //var y = visualXY.Select(_ => _.Y).ToArray();
-            //visualgraph.Series[1].Points.DataBindXY(x, y); // визуализация полного контура
+
 
             //PickKey(key);
-            ExcelArr = Interpretation.InterpretationOfCommands(SortedList, (double)height.Value, (double)heigthpause.Value).ToList();
-            infotime.Text = Interpretation.AllTime.Time.ToString();
+            // ExcelArr = Interpretation.InterpretationOfCommands(SortedList, (double)height.Value, (double)heigthpause.Value).ToList();
+            // infotime.Text = Interpretation.AllTime.Time.ToString();
         }
 
         private void PickKey(IList<Point> key)
         {
-
             var x = key.Select(_ => _.X).ToArray();
             infopoint.Text = x.Length.ToString();
             var y = key.Select(_ => _.Y).ToArray();
@@ -117,11 +117,9 @@ namespace CVTK
             }
 
         }
-
         void VisualPoint(TreeNodeCollection nodes)
         {
             List<Point> point = new List<Point>();
-            string buf = "";
             foreach (TreeNode parent in nodes) // уровень контуров
             {
                 if (parent.Checked == false)
@@ -130,8 +128,7 @@ namespace CVTK
                     {
                         if (child.Checked)
                         {
-                            buf = child.Text;
-                            var sp = buf.Split(';');
+                            var sp = child.Text.Split(';');
                             point.Add(new Point(Convert.ToInt32(sp[0]), Convert.ToInt32(sp[1])));
                         }
                         else { continue; }
@@ -142,15 +139,14 @@ namespace CVTK
                     foreach (TreeNode child in parent.Nodes) // уровень точек контура 
                     {
                         child.Checked = true;
-                        buf = child.Text;
-                        var sp = buf.Split(';');
+                        var sp = child.Text.Split(';');
                         point.Add(new Point(Convert.ToInt32(sp[0]), Convert.ToInt32(sp[1])));
                     }
                 }
             }
             PickKey(point);
-        }
 
+        }
         void nulls(TreeNodeCollection nodes)
         {
             foreach (TreeNode parent in nodes) // уровень контуров
@@ -301,61 +297,52 @@ namespace CVTK
         }
 
         //---------------------------------------Модуль для обработки Accord (ключевых точек)---------------------------------------//
-        void CornerFound(Bitmap input)
+
+        void CornerFound(params object[] list)
         {
-            double sigma = (double)numSigma.Value;
-            float k = (float)numK.Value;
-            float threshold = (float)numThreshold.Value;
-
-            // Create a new Harris Corners Detector using the given parameters
-            HarrisCornersDetector harris = new HarrisCornersDetector(k)
+            if (list.Length != 1)
             {
-                Threshold = threshold,
-                Sigma = sigma
-            };
-            // Create a new AForge's Corner Marker Filter
-            CornersMarker corners = new CornersMarker(harris, Color.White);
-            // input.RotateFlip(RotateFlipType.Rotate180FlipX);
-            var OriginCorner = harris.ProcessImage(input); // ОРИГИНАЛ
-            var CornerRrev = corners.Apply(input);
-            var Picture = GrayImg.ResizeImg(pictureBox1.Height, pictureBox1.Width);// подгонка для первоначального показа
-            Bitmap endpick = new Bitmap(CornerRrev, new Size(Picture.Item2, Picture.Item1));
-            pictureBox1.Image = endpick;
-
-
-         
-            
-          
-
-            listView1.Columns.Add("№", 75, HorizontalAlignment.Left);
-            listView1.Columns.Add("X", 40, HorizontalAlignment.Left);
-            listView1.Columns.Add("Y", 40, HorizontalAlignment.Left);
-            listView1.Columns.Add("Является точкой", 105, HorizontalAlignment.Left);
-            listView1.Columns.Add("Класс", 50, HorizontalAlignment.Left);
-
-            ListViewGroup g1 = new ListViewGroup("Контрольные точки");
-            listView1.Groups.Add(g1);
-
-            for (int i = 0; i < OriginCorner.Count; i++)
-            {
-                ListViewItem list = new ListViewItem(g1);
-                list.Text = i.ToString();
-                list.SubItems.Add(OriginCorner[i].X.ToString());
-                list.SubItems.Add(OriginCorner[i].Y.ToString());
-                listView1.Items.Add(list);
+                double sigma = (double)list[3];
+                double k = (double)list[1];
+                int threshold = (int)list[2];
+                // Create a new Harris Corners Detector using the given parameters
+                HarrisCornersDetector harris = new HarrisCornersDetector((float)k)
+                {
+                    Threshold = (float)threshold,
+                    Sigma = sigma
+                };
+                // Create a new AForge's Corner Marker Filter
+                CornersMarker corners = new CornersMarker(harris, Color.White);
+                // input.RotateFlip(RotateFlipType.Rotate180FlipX);
+                var OriginCorner = harris.ProcessImage((Bitmap)list[0]); // ОРИГИНАЛ
+                var CornerRrev = corners.Apply((Bitmap)list[0]);
+                var Picture = GrayImg.ResizeImg(pictureBox1.Height, pictureBox1.Width);// подгонка для первоначального показа
+                Bitmap endpick = new Bitmap(CornerRrev, new Size(Picture.Item2, Picture.Item1));
+                pictureBox1.Image = endpick;
+                CreateTable(OriginCorner);// построение таблицы key-точек 
             }
-            listView1.CheckBoxes = true;
-
-
-
-
-
-
-
-            var FakeCorner = harris.ProcessImage(endpick); // Уменьшеное
-
+            else
+            {
+                double sigma = (double)numSigma.Value;
+                float k = (float)numK.Value;
+                float threshold = (float)numThreshold.Value;
+                // Create a new Harris Corners Detector using the given parameters
+                HarrisCornersDetector harris = new HarrisCornersDetector(k)
+                {
+                    Threshold = threshold,
+                    Sigma = sigma
+                };
+                // Create a new AForge's Corner Marker Filter
+                CornersMarker corners = new CornersMarker(harris, Color.White);
+                // input.RotateFlip(RotateFlipType.Rotate180FlipX);
+                var OriginCorner = harris.ProcessImage((Bitmap)list[0]); // ОРИГИНАЛ
+                var CornerRrev = corners.Apply((Bitmap)list[0]);
+                var Picture = GrayImg.ResizeImg(pictureBox1.Height, pictureBox1.Width);// подгонка для первоначального показа
+                Bitmap endpick = new Bitmap(CornerRrev, new Size(Picture.Item2, Picture.Item1));
+                pictureBox1.Image = endpick;
+                CreateTable(OriginCorner);// построение таблицы key-точек 
+            }
         }
-
         private void Detect_Click(object sender, EventArgs e)
         {
             var ImgCorner = img2.Bitmap;
@@ -363,6 +350,63 @@ namespace CVTK
 
         }
 
-       
+        //---------------------------------------Модуль для обработки таблицы key-точек---------------------------------------//
+        void CreateTable(List<Accord.IntPoint> KeyList)
+        {
+            listView1.Clear();
+            listView1.Columns.Add("№", 75, HorizontalAlignment.Left);
+            listView1.Columns.Add("X", 40, HorizontalAlignment.Left);
+            listView1.Columns.Add("Y", 40, HorizontalAlignment.Left);
+            listView1.Columns.Add("Является точкой", 105, HorizontalAlignment.Left);
+            listView1.Columns.Add("Класс", 50, HorizontalAlignment.Left);
+            ListViewGroup g1 = new ListViewGroup("Контрольные точки");
+            listView1.Groups.Add(g1);
+
+            var copyarr = Concurrences(tree.Nodes);
+
+            for (int i = 0; i < copyarr.; i++)
+            {
+
+                for (int j = 0; j < copyarr.Count; j++)
+                {
+                }
+
+
+            }
+
+            for (int i = 0; i < KeyList.Count; i++)
+            {
+                ListViewItem list = new ListViewItem(g1);
+                list.Text = i.ToString();
+                list.SubItems.Add(KeyList[i].X.ToString());
+                list.SubItems.Add(KeyList[i].Y.ToString());
+                listView1.Items.Add(list);
+            }
+            listView1.CheckBoxes = true;
+
+        }
+
+        List<ContourFromTree> Concurrences(TreeNodeCollection nodes)
+        {
+            List<ContourFromTree> point = new List<ContourFromTree>();
+
+            foreach (TreeNode parent in nodes) // уровень контуров
+            {
+                ContourFromTree dot = new ContourFromTree();
+                dot.namecontour = parent.Text.ToString();
+                var result = new List<Point>();
+
+                foreach (TreeNode child in parent.Nodes) // уровень точек контура 
+                {
+                    var sp = child.Text.Split(';');
+
+                    result.Add(new Point(Convert.ToInt32(sp[0]), Convert.ToInt32(sp[1])));
+                    dot.Contr = result;
+
+                }
+                point.Add(dot);
+            }
+            return point;
+        }
     }
 }
